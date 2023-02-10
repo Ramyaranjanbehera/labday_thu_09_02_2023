42e5b950908abfae2e1ccb9eb6ac038c04846fc5
Ramyaranjana-behera <ramyaranjanbehera4@gmail.com>
2023-02-10 14:54:00 +0530

    first commit

---
+++ src/lab_day_Thu_09_02_2023/Que_no_01.java
@@ -0,0 +1,17 @@
+package lab_day_Thu_09_02_2023;
+
+public class Que_no_01 {
+public static void main(String[] args) {
+
+	        String name = "7735539064";
+	       
+//	        int value = name.length(); 
+//	        System.out.println(name.replace("7", "x"));
+	        String name1 = name.replace("7", "x");
+//	        System.out.println(name1.replace("3","x"));
+	        String name2 = name1.replace("3", "x");
+	        System.out.println(name2.replace("5", "x"));      
+
+
+	    }
+	}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ramya
+ *
+ */
+module lab_day_Thu_09_02_2023 {
+}
